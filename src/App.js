126e04ab301740ea1a9e65d18d108cfa0d91f7c5
Hemanth Vundavilli <118917390+Hemu2503@users.noreply.github.com>
2024-10-23 12:47:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import KanbanBoard from './../src/components/KanbanBoard';
 import './App.css';
 
 function App() {
+  const [tickets, setTickets] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [grouping, setGrouping] = useState('status'); // Default grouping
+  const [sortOrder, setSortOrder] = useState('priority'); // Default sorting
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.quicksell.co/v1/internal/frontend-assignment');
+        const data = await response.json();
+        setTickets(data.tickets);
+        setUsers(data.users);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <KanbanBoard
+        tickets={tickets}
+        users={users}
+        grouping={grouping}
+        setGrouping={setGrouping}
+        sortOrder={sortOrder}
+        setSortOrder={setSortOrder}
+      />
     </div>
   );
 }
